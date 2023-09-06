509c878bf6953b817bb1ce9bed21758f3a29cd4e
redstone-dev <31410740+redstone-dev@users.noreply.github.com>
2023-09-06 19:46:12 -0400

    npm isnt working ;-;

---
+++ index.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true // Allows you to use Node.js in the renderer process
+    }
+  });
+
+  mainWindow.loadFile('index.html'); // Load your HTML file
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
